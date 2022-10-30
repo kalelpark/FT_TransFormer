bef28a1f5e467bdca1b3b1cecf10914f82a86812
kalelpark <qkrdnjsrl0628@naver.com>
2022-10-30 03:59:14 +0000

    update 10.30

--- experiment.sh
+++ experiment.sh
@@ -8,20 +8,19 @@
 
 # If you any Question, leave an Issue.
 
-# TIP!
 # FT-Transformer
 
 # Regression
-# python main.py --action train --model fttransformer --data california_housing --savepath output         # 0.01
-# python main.py --action train --model fttransformer --data microsoft --savepath output                  # 0.03
-# python main.py --action train --model fttransformer --data yahoo --savepath output                      
-# python main.py --action train --model fttransformer --data year --savepath output 
+# python main.py --action train --model fttransformer --data california_housing --savepath output         # Clear
+# python main.py --action train --model fttransformer --data microsoft --savepath output                  # Clear
+# python main.py --action train --model fttransformer --data yahoo --savepath output                      # Clear
+# python main.py --action train --model fttransformer --data year --savepath output                       # Clear
 
 # Multi classification
-# python main.py --action train --model fttransformer --data aloi --savepath output                        # 0.06
-# python main.py --action train --model fttransformer --data covtype --savepath output                     # 0.005
-# python main.py --action train --model fttransformer --data helena --savepath output                      # 0.016
-# python main.py --action train --model fttransformer --data jannis --savepath output
+# python main.py --action train --model fttransformer --data aloi --savepath output                        # Clear
+# python main.py --action train --model fttransformer --data covtype --savepath output                     # Clear
+# python main.py --action train --model fttransformer --data helena --savepath output                      # Clear
+# python main.py --action train --model fttransformer --data jannis --savepath output                      # Clear
 
 # Binary Classification
 # python main.py --action train --model fttransformer --data epsilon --savepath output                   
@@ -31,16 +30,16 @@
 # ResNet
 
 # Regression
-# python main.py --action train --model resnet --data california_housing --savepath output         +- 0.01
-# python main.py --action train --model resnet --data microsoft --savepath output
-# python main.py --action train --model resnet --data yahoo --savepath output
-# python main.py --action train --model resnet --data year --savepath output 
+# python main.py --action train --model resnet --data california_housing --savepath output                 # Clear       
+# python main.py --action train --model resnet --data microsoft --savepath output                          # Clear
+# python main.py --action train --model resnet --data yahoo --savepath output                              # Clear
+# python main.py --action train --model resnet --data year --savepath output                               # Clear
 
 # Multi classification
-# python main.py --action train --model resnet --data aloi --savepath output                      
-# python main.py --action train --model resnet --data covtype --savepath output
-# python main.py --action train --model resnet --data helena --savepath output
-# python main.py --action train --model resnet --data jannis --savepath output
+# python main.py --action train --model resnet --data aloi --savepath output                            # Clear              
+# python main.py --action train --model resnet --data covtype --savepath output                         # Clear
+# python main.py --action train --model resnet --data helena --savepath output                          # Clear
+# python main.py --action train --model resnet --data jannis --savepath output                          # Clear
 
 # Binary Classification
 # python main.py --action train --model resnet --data epsilon --savepath output                   
